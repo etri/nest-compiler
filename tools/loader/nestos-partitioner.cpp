b3f4bd029008173b3c3f920fc80a1254d7c8a01b
Misun Yu <msyu@etri.re.kr>
2022-03-20 14:52:38 +0900

    update license headers for the C-Code-generator and the PartitionTuner
    Signed-off-by: Misun Yu <msyu@etri.re.kr>

--- tools/loader/nestos-partitioner.cpp
+++ tools/loader/nestos-partitioner.cpp
@@ -1,19 +1,20 @@
-/**
- * Copyright (c) Glow Contributors. See CONTRIBUTORS file.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * Modifications copyright (C) 2022 <ETRI/Yongin Kwon>
- */
+/*****************************************************************************
+	*
+	* Copyright Next-Generation System Software Research Group, All rights reserved.
+	* Future Computing Research Division, Artificial Intelligence Reserch Laboratory
+	* Electronics and Telecommunications Research Institute (ETRI)
+	*
+	* THESE DOCUMENTS CONTAIN CONFIDENTIAL INFORMATION AND KNOWLEDGE
+	* WHICH IS THE PROPERTY OF ETRI. NO PART OF THIS PUBLICATION IS
+	* TO BE USED FOR ANY OTHER PURPOSE, AND THESE ARE NOT TO BE"
+	* REPRODUCED, COPIED, DISCLOSED, TRANSMITTED, STORED IN A RETRIEVAL
+	"* SYSTEM OR TRANSLATED INTO ANY OTHER HUMAN OR COMPUTER LANGUAGE,
+	* IN ANY FORM, BY ANY MEANS, IN WHOLE OR IN PART, WITHOUT THE
+	* COMPLETE PRIOR WRITTEN PERMISSION OF ETRI.
+	*
+	* LICENSE file : README_LICENSE_ETRI located in the top directory
+	*
+*****************************************************************************/
 
 #include <boost/algorithm/string.hpp>
 #include <iostream>
